73daadca1d7d38ec1294dca85b10d70582cc0388
fahsai <fahsai.srip@gmail.com>
2024-03-08 12:41:18 +0700

    fix: install @line/liff

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const config = {
+  appEnv: process.env.NEXT_PUBLIC_APPLICATION_ENV || "development",
+  liffId: process.env.NEXT_PUBLIC_LIFF_ID || "development",
+};
+
+export default config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import config from "@/config";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const liffInit = async () => {
+    const liff = (await import("@line/liff")).default;
+
+    try {
+      await liff.init({ liffId: config.liffId });
+    } catch (error) {
+      console.error("liff init error", error);
+    }
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  };
+
+  useEffect(() => {
+    liffInit();
+  }, []);
+
   return <Component {...pageProps} />;
 }
