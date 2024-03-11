fc722ae8aa0e6da27de62ccf7d2a2486fb46e8f0
fahsai <fahsai.srip@gmail.com>
2024-03-11 11:27:29 +0700

    add .env.exemple

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   appEnv: process.env.NEXT_PUBLIC_APPLICATION_ENV || "development",
-  liffId: process.env.NEXT_PUBLIC_LIFF_ID || "development",
+  liffId: process.env.NEXT_PUBLIC_LIFF_ID || "",
 };
 
 export default config;
